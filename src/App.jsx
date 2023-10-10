4ae7635c4734e5636d0491dd1a5a0307da4fbaba
Marcos Coelho <mparav124@gmail.com>
2023-10-10 01:38:32 -0300

    add: confetti

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import './App.css';
 import Profile from './components/profile/Profile';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
+import Confetti from 'react-confetti';
+import React from 'react';
 
 function App() {
+  const [confettiOn, setConfettiOn] = React.useState(false);
+
   return (
     <>
+      {confettiOn && (
+        <Confetti
+          height={1220}
+          gravity={0.3}
+          // tweenDuration={1000}
+          // initialVelocityY={60}
+          recycle={false}
+          onConfettiComplete={() => setConfettiOn(false)}
+        />
+      )}
       <main>
-        <Profile />
+        <Profile setConfetti={setConfettiOn} />
         <About />
         <Projects />
       </main>
